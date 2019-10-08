ab54a851a9e30f08da0c84e2632ac1d95653f982
Yilong Li <yilongl@cs.stanford.edu>
2019-10-08 17:41:50 +0800

    millisort: updated perf. dashboard to compute time breakdown using the largest start/stop time among servers

--- src/MilliSortService.cc
+++ src/MilliSortService.cc
@@ -891,8 +891,8 @@ MilliSortService::pivotBucketSort()
     // bucket boundaries.
     timeTrace("about to shuffle pivots");
     {
-        ADD_COUNTER(bucketSortPivotsStartTime, Cycles::rdtsc() - startTime);
-        SCOPED_TIMER(bucketSortPivotsElapsedTime);
+        ADD_COUNTER(shufflePivotsStartTime, Cycles::rdtsc() - startTime);
+        SCOPED_TIMER(shufflePivotsElapsedTime);
         uint32_t cmlSmallerPivots = 0;
         uint32_t numLocalPivots = downCast<uint32_t>(gatheredPivots.size());
         for (int rank = 0; rank < allPivotServers->size(); rank++) {
@@ -1079,6 +1079,7 @@ MilliSortService::startMergeSorter()
 {
     auto mergeSortPollerMain = [this] () {
         uint64_t pollerStartTime = Cycles::rdtsc();
+        ADD_COUNTER(onlineMergeSortPollerStartTime, pollerStartTime - startTime)
         bool done = false;
         while (!done) {
             mergeSorterLock.lock();
