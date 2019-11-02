yilongli/RAMCloud

commit e691072cc947126081e7036efaa2845a3b3b9b63

millisort: make ShufflePushRpc a direct subclass of RpcWrapper

src/MilliSortService.cc
@@ -115,6 +115,8 @@ namespace {
 // Change 0 -> 1 to enable final result validation.
 #define VALIDATE_RESULT 1
 
+uint32_t MilliSortService::MAX_SHUFFLE_CHUNK_SIZE = 40000;
+
 /**
  * Construct an MilliSortService.
  *
@@ -168,6 +170,7 @@ MilliSortService::MilliSortService(Context* context,
     , mergeSorterLock("MilliSortService::mergeSorter")
     , pendingMergeReqs()
     , world()
+    , sessions()
     , myPivotServerGroup()
     , gatheredPivots()
     , superPivots()
@@ -203,6 +206,13 @@ MilliSortService::MilliSortService(Context* context,
             uint64_t(block.get()) + block.length) {
         DIE("sortedValues0 outside the range of allocated memory!");
     }
+
+    char* str = std::getenv("MILLISORT_SHUFFLE_CHUNK_SIZE");
+    if (str) {
+        std::string val(str);
+        MAX_SHUFFLE_CHUNK_SIZE = std::stoul(val);
+    }
+    LOG(NOTICE, "MAX_SHUFFLE_CHUNK_SIZE = %u", MAX_SHUFFLE_CHUNK_SIZE);
 }
 
 MilliSortService::~MilliSortService()
@@ -303,6 +313,10 @@ MilliSortService::initMilliSort(const WireFormat::InitMilliSort::Request* reqHdr
     // Initialize the world communication group so we know the rank of this
     // server.
     initWorld(reqHdr->numNodes);
+    sessions.clear();
+    for (int i = 0; i < world->size(); i++) {
+        sessions.push_back(context->serverList->getSession(world->getNode(i)));
+    }
 
     // Initialization request from an external client should only be processed
     // by the root node, which also performs error checking.
@@ -1388,6 +1402,9 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
         /// Server ID of the message receiver.
         ServerId serverId;
 
+        /// Session to the receiver.
+        Transport::SessionRef session;
+
         /// Size of the complete message, in bytes.
         uint32_t totalLength;
 
@@ -1400,9 +1417,11 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
         /// Time, in rdtsc ticks, when we send out #outstandingRpc.
         uint64_t lastTransmitTime;
 
-        ShuffleMessageTracker(ServerId serverId, Buffer* message)
+        ShuffleMessageTracker(ServerId serverId, Transport::SessionRef session,
+                Buffer* message)
             : outstandingRpc()
             , serverId(serverId)
+            , session(session)
             , totalLength(message->size())
             , offset(0)
             , messageIt(message)
@@ -1422,7 +1441,7 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
             size = std::min(size, messageIt.size());
             Buffer::Iterator payload(messageIt);
             payload.truncate(size);
-            outstandingRpc.construct(context, serverId, senderId, dataId,
+            outstandingRpc.construct(context, session, senderId, dataId,
                     totalLength, offset, &payload);
             timeTraceSp("shuffle-push: push rpc constructed, rpcId %u",
                     outstandingRpc->rpcId);
@@ -1439,15 +1458,15 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
             new Tub<ShuffleMessageTracker>[totalMessages]);
     for (int i = 0; i < totalMessages; i++) {
         Buffer* message = &(*outMessages)[i];
-        messageTrackers[i].construct(group->getNode(group->rank + i), message);
+        ServerId serverId = group->getNode(group->rank + i);
+        messageTrackers[i].construct(serverId,
+                sessions[serverId.indexNumber()-1], message);
         totalBytesSent += message->size();
     }
 
     // Performance stats.
     uint64_t shuffleStart = Cycles::rdtsc();
     uint32_t outstandingChunks = 0;
-
-#define MAX_PUSH_CHUNK_SIZE 40000
 
     std::vector<int> candidates;
     int messagesUnfinished = totalMessages;
@@ -1582,8 +1601,8 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
             }
             // TODO: incast-control? for the first chunk, make it smaller
 //            uint32_t chunkSize = messageTrackers[index]->offset > 0 ?
-//                    MAX_PUSH_CHUNK_SIZE : 8000;
-            uint32_t chunkSize = MAX_PUSH_CHUNK_SIZE;
+//                    MAX_SHUFFLE_CHUNK_SIZE : 1000;
+            uint32_t chunkSize = MAX_SHUFFLE_CHUNK_SIZE;
             uint64_t now = Cycles::rdtsc();
             messageTrackers[index]->lastTransmitTime = now;
             messageTrackers[index]->sendMessageChunk(context, group->rank,

src/MilliSortClient.cc
@@ -13,6 +13,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include "ClientException.h"
 #include "MilliSortClient.h"
 #include "Util.h"
 
@@ -139,15 +140,16 @@ SendDataRpc::wait()
     return response;
 }
 
-ShufflePushRpc::ShufflePushRpc(Context* context, ServerId serverId,
+ShufflePushRpc::ShufflePushRpc(Context* context, Transport::SessionRef session,
         int32_t senderId, uint32_t dataId, uint32_t totalLength,
         uint32_t offset, Buffer::Iterator* payload)
-    : ServerIdRpcWrapper(context, serverId,
-            sizeof(WireFormat::ShufflePush::Response))
+    : RpcWrapper(responseHeaderLength)
+    , context(context)
     , rpcId(Util::wyhash64())
 {
     appendRequest(&request, senderId, dataId, totalLength, offset, payload,
             rpcId);
+    this->session = session;
     send();
 }
 
@@ -168,6 +170,22 @@ ShufflePushRpc::appendRequest(Buffer* request, int32_t senderId,
         request->appendExternal(payload->getData(), payload->getLength());
         payload->next();
     }
+}
+
+void
+ShufflePushRpc::wait()
+{
+    waitInternal(context->dispatch);
+    if (getState() != RpcState::FINISHED) {
+        throw TransportException(HERE);
+    }
+
+    const WireFormat::ShufflePush::Response* respHdr(
+            getResponseHeader<WireFormat::ShufflePush>());
+
+    if (respHdr->common.status != STATUS_OK)
+        ClientException::throwException(HERE, respHdr->common.status);
+    response->truncateFront(responseHeaderLength);
 }
 
 }  // namespace RAMCloud

src/MilliSortClient.h
@@ -143,21 +143,23 @@ PRIVATE:
     DISALLOW_COPY_AND_ASSIGN(SendDataRpc);
 };
 
-class ShufflePushRpc : public ServerIdRpcWrapper {
+class ShufflePushRpc : public RpcWrapper {
   public:
-    ShufflePushRpc(Context* context, ServerId serverId, int32_t senderId,
-            uint32_t dataId, uint32_t totalLength, uint32_t offset,
-            Buffer::Iterator* payload);
+    ShufflePushRpc(Context* context, Transport::SessionRef session,
+            int32_t senderId, uint32_t dataId, uint32_t totalLength,
+            uint32_t offset, Buffer::Iterator* payload);
     ~ShufflePushRpc() {}
 
     static void appendRequest(Buffer* request, int32_t senderId,
             uint32_t dataId, uint32_t totalLength, uint32_t offset,
             Buffer::Iterator* payload, uint32_t rpcId);
 
-    void wait() { waitAndCheckErrors(); }
+    void wait();
 
     static const uint32_t responseHeaderLength =
             sizeof(WireFormat::ShufflePush::Response);
+
+    Context* context;
 
     // FIXME: Debug only; remove?
     uint32_t rpcId;

src/MilliSortService.h
@@ -186,6 +186,8 @@ class MilliSortService : public Service {
 
     static const int MAX_IMBALANCE_RATIO = 2;
 
+    static uint32_t MAX_SHUFFLE_CHUNK_SIZE;
+
     /**
      * A pivot consists of the original key of the data tuple plus some
      * metadata.
@@ -736,6 +738,9 @@ class MilliSortService : public Service {
 
     /// Contains all nodes in the service.
     Tub<CommunicationGroup> world;
+
+    /// Sessions to all nodes in #world.
+    std::vector<Transport::SessionRef> sessions;
 
     /// Contains the local node, the pivot server it's assigned to (or the local
     /// node itself is a pivot server), and all other nodes that are assigned to

commit 56110ee85673f7cf2e87b27ca707da1960cc55a9

shuffle: fixed the perf. regression bug since e691072
The candidate set is incorrectly computed to include finished messages.

src/MilliSortService.cc
@@ -1665,12 +1665,11 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
         }
     };
 
-//    uint64_t activeCycles = context->dispatch->dispatchActiveCycles;
-//    uint64_t totalBytesSent = 0;
+    uint64_t activeCycles = context->dispatch->dispatchActiveCycles;
+    uint64_t totalBytesSent = 0;
 
-    // FIXME: docs
 #if !PIGGYBACK_SHUFFLE_MSG
-    int numOutMessages = group->size()
+    int numOutMessages = group->size();
 #else
     int numOutMessages = (group->size() + 1) / 2;
     if (group->size() % 2 == 0) {
@@ -1689,7 +1688,7 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
         ServerId serverId = group->getNode(group->rank + i);
         messageTrackers[i].construct(serverId,
                 sessions[serverId.indexNumber()-1], message);
-//        totalBytesSent += message->size();
+        totalBytesSent += message->size();
     }
 
     // Performance stats.
@@ -1759,7 +1758,6 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
                         messageTracker->receiveOffset += reply->size();
                     }
 #endif
-
                     messageTracker->outstandingRpc.destroy();
                     if (messageTracker->isDone()) {
                         messagesUnfinished--;
@@ -1774,7 +1772,10 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
                     continue;
                 }
             }
-            candidates.push_back(i);
+
+            if (messageTrackers[i]->getBytesLeft() > 0) {
+                candidates.push_back(i);
+            }
         }
 
         // TODO: what should be a healthy amout of pending RPCs?
@@ -1785,8 +1786,9 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
         // Power-of-two LRPT policy.
         int index = -1;
         uint32_t maxBytesLeft = 0;
-        if (!candidates.empty() && (outstandingChunks < maxOutstandingChunks)
-                && (transmitPendingRpcs < maxTxPendingRpcs)) {
+        bool readyToSend = (outstandingChunks < maxOutstandingChunks) &&
+                (transmitPendingRpcs < maxTxPendingRpcs);
+        if (!candidates.empty() && readyToSend) {
             for (int choice = 0; choice < maxChoices; choice++) {
                 int idx = candidates[Util::wyhash64() % candidates.size()];
                 uint32_t bytesLeft = messageTrackers[idx]->getBytesLeft();
@@ -1798,14 +1800,7 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
         }
 
         // Send out one more chunk of data, if appropriate.
-        if ((transmitPendingRpcs < maxTxPendingRpcs) &&
-                (outstandingChunks < maxOutstandingChunks) && (index >= 0)) {
-            if ((transmitPendingRpcs + 1 == maxTxPendingRpcs) ||
-                    (outstandingChunks + 1 == maxOutstandingChunks)) {
-                timeTraceSp("shuffle: ready to send again, "
-                        "transmitPendingRpcs %u, outstandingChunks %u",
-                        transmitPendingRpcs, outstandingChunks);
-            }
+        if (readyToSend && (index >= 0)) {
             uint64_t now = Cycles::rdtsc();
             messageTrackers[index]->lastTransmitTime = now;
             messageTrackers[index]->sendMessageChunk(context, group->rank,
@@ -1822,14 +1817,16 @@ MilliSortService::invokeShufflePush(CommunicationGroup* group, uint32_t dataId,
         Arachne::yield();
     }
 
-//    activeCycles = context->dispatch->dispatchActiveCycles - activeCycles;
-//    uint64_t elapsed = Cycles::rdtsc() - shuffleStart;
-//    timeTraceSp("shuffle: operation completed, idle %u us, active %u us, "
-//            "active BW %u Gbps, actual BW %u Gbps",
-//            Cycles::toMicroseconds(elapsed - activeCycles),
-//            Cycles::toMicroseconds(activeCycles),
-//            uint32_t(double(totalBytesSent)*8e-9/Cycles::toSeconds(activeCycles)),
-//            uint32_t(double(totalBytesSent)*8e-9/Cycles::toSeconds(elapsed)));
+    activeCycles = context->dispatch->dispatchActiveCycles - activeCycles;
+    uint64_t elapsed = Cycles::rdtsc() - shuffleStart;
+#if !PIGGYBACK_SHUFFLE_MSG
+    timeTraceSp("shuffle: operation completed, idle %u us, active %u us, "
+            "active BW %u Gbps, actual BW %u Gbps",
+            Cycles::toMicroseconds(elapsed - activeCycles),
+            Cycles::toMicroseconds(activeCycles),
+            uint32_t(double(totalBytesSent)*8e-9/Cycles::toSeconds(activeCycles)),
+            uint32_t(double(totalBytesSent)*8e-9/Cycles::toSeconds(elapsed)));
+#endif
 }
 
 void
@@ -1842,6 +1839,7 @@ MilliSortService::shufflePush(const WireFormat::ShufflePush::Request* reqHdr,
     const uint32_t chunkSize = reqHdr->chunkSize;
     const uint32_t senderId = downCast<uint32_t>(reqHdr->senderId);
     rpc->requestPayload->truncateFront(sizeof(*reqHdr));
+    respHdr->receiverId = world->rank;
 
     switch (reqHdr->dataId) {
         case ALLSHUFFLE_PIVOTS: {

src/WireFormat.h
@@ -445,6 +445,8 @@ struct ShufflePush {
     struct Response {
         ResponseCommon common;
 
+        // hack: for logging purpose
+        uint32_t receiverId;
         // # bytes of the message to be received from the server.
         uint32_t totalLength;
         uint32_t offset;